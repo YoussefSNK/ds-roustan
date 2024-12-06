18015dd08c063a9300167e250ba3c89f1b7a53ec
Youssef Fadili <myoussef.fadili@gmail.com>
2024-12-06 18:22:59 +0100

    feat: Matrix

--- crazy_file.py
+++ crazy_file.py
@@ -9,3 +9,20 @@ def several_zeros_custom(nb_zeros):
     print("0" * nb_zeros)
 
 several_zeros_custom(10)
+
+
+
+# à check plus tard
+def matrix(rows, cols):
+    liste = []
+    listea = []
+    for i in range(cols):
+        liste.append(0)
+    
+    print(liste)
+
+    for i in range(rows):
+        listea.append(liste)
+    print(listea)
+matrix(3,2)
+matrix(10,5)
