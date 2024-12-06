e92428ccce8ca792b40821201db756e3bc00e922
Youssef Fadili <myoussef.fadili@gmail.com>
2024-12-06 19:32:44 +0100

    feat: azdazfdazf

--- disk.py
+++ disk.py
@@ -14,13 +14,10 @@ class Disk:
     def used_perc(self):
         return self.used /self.total
 
+    def __lt__(self, other):
+            if not isinstance(other, Disk):
+                return NotImplemented
+            return self.used < other.used
+
 if __name__ == '__main__':
-    disk1 = Disk(total = 10, used = 2)
-    disk2 = Disk(total = 20, used = 5)
-    print(disk1.free) # 8
-    print(disk2.free) # 15
-    print(disk1.used_perc) # 0.2
-    print(disk2.used_perc) # 0.25
-    disks = [disk1, disk2]
-    # disks_sorted = sorted(disks)
     pass
